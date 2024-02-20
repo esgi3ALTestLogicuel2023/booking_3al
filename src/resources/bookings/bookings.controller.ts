354209e8a492d33c598a4807f66cd592d8b2816b
Willy Noubissie <willyjunior91@gmail.com>
2024-02-20 01:41:48 +0100

    start booking secure

--- src/resources/bookings/bookings.controller.ts
+++ src/resources/bookings/bookings.controller.ts
@@ -6,15 +6,19 @@ import {
   Param,
   Delete,
   Put,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
 import { CreateBookingDto } from './dto/create-booking.dto';
 import { UpdateBookingDto } from './dto/update-booking.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('bookings')
 export class BookingsController {
   constructor(private readonly bookingsService: BookingsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post() // <-- POST /bookings
   create(
     @Body()
@@ -23,16 +27,19 @@ export class BookingsController {
     return this.bookingsService.create(createBookingDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get() // <-- GET /bookings
   findAll() {
     return this.bookingsService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id') // <-- GET /bookings/1
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string, @Req() req: any) {
     return this.bookingsService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(
     @Param('id') id: string,
@@ -42,6 +49,7 @@ export class BookingsController {
     return this.bookingsService.update(id, updateBookingDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete('/:id/userId/:userId')
   remove(@Param('id') id: string, @Param('userId') userId: string) {
     return this.bookingsService.remove(id, userId);
